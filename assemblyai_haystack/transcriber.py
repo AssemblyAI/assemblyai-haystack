65ebf4ec7fd8250a0a31cc9e614f7038bd107ce3
bilgeyucel <bilgeyucel96@gmail.com>
2023-12-07 21:02:53 +0300

    Remove preview handle and apply Haystack api changes

--- assemblyai_haystack/transcriber.py
+++ assemblyai_haystack/transcriber.py
@@ -1,8 +1,6 @@
 from typing import Dict, List, Any, Optional
 
-from canals.serialization import default_to_dict, default_from_dict
-from haystack.preview import component, Document
-
+from haystack import component, Document, default_to_dict, default_from_dict
 from haystack.lazy_imports import LazyImport
 
 with LazyImport(message="Run 'pip install assemblyai'") as assemblyai_import:
@@ -16,13 +14,6 @@ class AssemblyAITranscriber:
         if api_key is not None:
             aai.settings.api_key = api_key
 
-    def to_dict(self) -> Dict[str, Any]:
-        return default_to_dict(self)
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "AssemblyAITranscriber":
-        return default_from_dict(cls, data)
-
     @component.output_types(
         transcription=List[Document],
         summarization=List[Document],
@@ -31,31 +22,30 @@ class AssemblyAITranscriber:
     def run(
         self,
         file_path: str,
-        summarization: Optional[bool] = False,
-        speaker_labels: Optional[bool] = False,
+        summarization: bool = False,
+        speaker_labels: bool = False,
     ):
-        self.file_path = file_path
-
         config = aai.TranscriptionConfig(
             speaker_labels=speaker_labels, summarization=summarization
         )
 
         # Instantiating the Transcriber will raise a ValueError if no API key is set.
         self.transcriber = aai.Transcriber(config=config)
-        transcript = self.transcriber.transcribe(self.file_path)
+        transcript = self.transcriber.transcribe(file_path)
 
         # Not doing TypeChecking here because there is only one type of Transcript Format
         if transcript.error:
             raise ValueError(f"Could not transcribe file: {transcript.error}")
 
+
+        summarization_doc = {}
         # Create summarization result doc
         if config.summarization:
             summarization_doc = {
                 "summarization": [Document(content=transcript.summary)]
             }
-        else:
-            summarization_doc = {}
-
+        
+        speakers_doc = {}
         # create speaker labels result doc
         if config.speaker_labels:
             speakers_doc = {
@@ -66,8 +56,7 @@ class AssemblyAITranscriber:
                     for utterance in transcript.utterances
                 ]
             }
-        else:
-            speakers_doc = {}
+
 
         # create transcription result doc
         transcription_doc = {
